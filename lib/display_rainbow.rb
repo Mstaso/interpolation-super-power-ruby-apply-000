24e3c4c14765a51b37b6716492fe17fe0d840515
malcolm staso <mstaso1@gmail.com>
2020-04-18 15:11:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,10 @@
 # Write your #display_rainbow method here
 display_rainbow (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-puts "the first letter is {red [0]} "
-puts "the second letter is {orange [1]}"
-puts "the third letter "
+puts "the first letter is {R [0]} "
+puts "the second letter is {O [1]}"
+puts "the third letter is {Y [2]}"
+puts "the fourth letter is {G [3]}"
+puts "the fifth letter is {B [4]}"
+puts "the sixth letter is {I [5]}"
+puts "the seventh letter is {V [6]}"
+end

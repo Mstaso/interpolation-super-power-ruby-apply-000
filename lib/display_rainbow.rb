ade90ea83789e494649692bf844c1b2aea6519cc
malcolm staso <mstaso1@gmail.com>
2020-04-18 15:28:55 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,12 +3,12 @@
 display_rainbow=["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def display_rainbow (color)
-puts "the first letter is {R: red [0]} "
-puts "the second letter is {O: orange [1]}"
-puts "the third letter is {Y: yellow [2]}"
-puts "the fourth letter is {G: green[3]}"
-puts "the fifth letter is {B: blue [4]}"
-puts "the sixth letter is {I: indigo [5]}"
-puts "the seventh letter is {V: violet [6]}"
+puts "the first color is {R: red [0]} "
+puts "the second color is {O: orange [1]}"
+puts "the third color is {Y: yellow [2]}"
+puts "the fourth color is {G: green[3]}"
+puts "the fifth color is {B: blue [4]}"
+puts "the sixth color is {I: indigo [5]}"
+puts "the seventh color is {V: violet [6]}"
 end
 
